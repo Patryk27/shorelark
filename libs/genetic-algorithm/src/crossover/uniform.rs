becbb924e7ec6f155cdb496976905d3cd74064bb
Patryk Wychowaniec <wychowaniec.patryk@gmail.com>
2021-03-06 14:50:56 +0100

    refactor(ga): Improve UniformCrossover's test

--- libs/genetic-algorithm/src/crossover/uniform.rs
+++ libs/genetic-algorithm/src/crossover/uniform.rs
@@ -32,17 +32,33 @@ mod tests {
 
     #[test]
     fn test() {
-        let parent_a = Genome::from_iter(vec![1.0, 2.0, 3.0, 4.0, 5.0]);
-        let parent_b = Genome::from_iter(vec![10.0, 20.0, 30.0, 40.0, 50.0]);
         let mut rng = ChaCha8Rng::from_seed(Default::default());
+        let parent_a = Genome::from_iter((0..100).map(|n| n as f32));
+        let parent_b = Genome::from_iter((100..200).map(|n| n as f32));
 
-        let actual: Vec<_> = UniformCrossover::new()
+        let child: Vec<_> = UniformCrossover::new()
             .crossover(&mut rng, &parent_a, &parent_b)
             .into_iter()
             .collect();
 
-        let expected = vec![10.0, 20.0, 3.0, 4.0, 5.0];
+        // Number of genes different between `child` and `parent_a`
+        let diff_a = child
+            .iter()
+            .zip(parent_a.iter())
+            .filter(|(&c, &p)| c != p)
+            .count();
 
-        approx::assert_relative_eq!(actual.as_slice(), expected.as_slice());
+        // Number of genes different between `child` and `parent_b`
+        let diff_b = child
+            .iter()
+            .zip(parent_b.iter())
+            .filter(|(&c, &p)| c != p)
+            .count();
+
+        // Roughly looks like 50%, which proves that chance for picking either
+        // gene *is* 50%
+        assert_eq!(diff_a, 49);
+        assert_eq!(diff_b, 51);
+        assert_eq!(diff_a + diff_b, 100);
     }
 }
