9426b2d2abb3effc1012c9ded9c27fdf66527f48
Patryk Wychowaniec <wychowaniec.patryk@gmail.com>
2021-03-13 15:13:06 +0100

    ga: Simplify Chromosome's tests

--- libs/genetic-algorithm/src/crossover/uniform.rs
+++ libs/genetic-algorithm/src/crossover/uniform.rs
@@ -33,37 +33,24 @@ mod tests {
     use super::*;
     use rand::SeedableRng;
     use rand_chacha::ChaCha8Rng;
-    use std::iter::FromIterator;
 
     #[test]
     fn test() {
         let mut rng = ChaCha8Rng::from_seed(Default::default());
-        let parent_a = Chromosome::from_iter((0..100).map(|n| n as f32));
-        let parent_b = Chromosome::from_iter((100..200).map(|n| n as f32));
+        let parent_a: Chromosome = (1..=100).map(|n| n as f32).collect();
+        let parent_b: Chromosome = (1..=100).map(|n| -n as f32).collect();
 
-        let child: Vec<_> = UniformCrossover::new()
-            .crossover(&mut rng, &parent_a, &parent_b)
-            .into_iter()
-            .collect();
+        let child = UniformCrossover::new().crossover(&mut rng, &parent_a, &parent_b);
 
         // Number of genes different between `child` and `parent_a`
-        let diff_a = child
-            .iter()
-            .zip(parent_a.iter())
-            .filter(|(&c, &p)| c != p)
-            .count();
+        let diff_a = child.iter().zip(parent_a).filter(|(c, p)| *c != p).count();
 
         // Number of genes different between `child` and `parent_b`
-        let diff_b = child
-            .iter()
-            .zip(parent_b.iter())
-            .filter(|(&c, &p)| c != p)
-            .count();
+        let diff_b = child.iter().zip(parent_b).filter(|(c, p)| *c != p).count();
 
         // Roughly looks like 50%, which proves that chance for picking either
         // gene *is* 50%
         assert_eq!(diff_a, 49);
         assert_eq!(diff_b, 51);
-        assert_eq!(diff_a + diff_b, 100);
     }
 }
